74146f8de828f2da84ef1d7cbdad0b6450f171bd
Samith Basnayaka <bmsd.basnayaka@gmail.com>
2017-10-31 11:21:56 +0530

    updating files

--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,11 @@
 
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
-var LocalStrategy    = require('passport-local').Strategy;    //Oauth
-var FacebookStrategy = require('passport-facebook').Strategy;//Oauth
-var TwitterStrategy  = require('passport-twitter').Strategy;//Oauth
+var LocalStrategy    = require('passport-local').Strategy;           //Oauth
+var FacebookStrategy = require('passport-facebook').Strategy;        //Oauth
+var TwitterStrategy  = require('passport-twitter').Strategy;         //Oauth
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;//Oauth
+
 
 // load up the user model
 var mysql = require('mysql');
@@ -35,7 +36,7 @@ module.exports = function(passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        connection.query("SELECT * FROM login WHERE id = ? ",[id], function(err, rows){
+        connection.query("SELECT * FROM login WHERE idlogin = ? ",[id], function(err, rows){
             done(err, rows[0]);
         });
     });
@@ -50,34 +51,33 @@ module.exports = function(passport) {
         'local-signup',
         new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            
             usernameField : 'username',
             passwordField : 'password',
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
-        function(req, username, password, done) {
+        function(req, username, password, lname, fname, done) {
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
+            var newUserMysql = {
+                        username: username,
+                        password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
+                    };
+
             connection.query("SELECT * FROM login WHERE username = ?",[username], function(err, rows) {
                 if (err)
                     return done(err);
                 if (rows.length) {
                     return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
                 } else {
-                    // if there is no user with that username
-                    // create the user
-                    var newUserMysql = {
-                        username: username,
-                        password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
-                    };
-
-                    var insertQuery = "INSERT INTO login ( username, password ) values (?,?)";
-
-                    connection.query(insertQuery,[newUserMysql.username, newUserMysql.password],function(err, rows) {
-                        newUserMysql.id = rows.insertId;
-
-                        return done(null, newUserMysql);
+                    
+                        
+                          console.log("Connected!");
+                          var insertQuery = "INSERT INTO login ( username, password ) values (?,?)";
+                           connection.query(insertQuery,[newUserMysql.username, newUserMysql.password],function(err, rows) {
+                          if (err) throw err;
+                            console.log("1 record inserted");
                     });
+
                 }
             });
         })
@@ -109,17 +109,22 @@ module.exports = function(passport) {
                 if (!bcrypt.compareSync(password, rows[0].password))
                     return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
+                console.log("user found");
                 // all is well, return successful user
                 return done(null, rows[0]);
             });
         })
     );
 
+
+
+
+
     //-----------------------------------------------------------Oauth.......................................................................................
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
-        done(null, user.id);
+        done(null, 3);
     });
 
     // used to deserialize the user
