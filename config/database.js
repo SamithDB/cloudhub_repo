3b0b206d51fe602acae0f204634823fe86cf807e
Samith Basnayaka <bmsd.basnayaka@gmail.com>
2018-04-18 15:25:57 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': '35.200.157.12',
         'user': 'root',
-        'password': 'admin123'
+        'password': ''
     },
 	'database': 'EmployeePortal_DB',
     'users_table': 'login'
